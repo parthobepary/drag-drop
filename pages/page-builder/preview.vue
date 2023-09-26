bfa0c7cc2257ccfc4bdd30579ea15208d25de6be
beparypartho <pallobbiswas326@gmail.com>
2023-09-26 11:20:02 +0600

    init

---
+++ pages/page-builder/preview.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container mx-auto">
+        <section class="w-full px-8 text-gray-700 bg-[#F3F4F6]">
+            <div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
+                <div class="relative flex flex-col md:flex-row">
+                    <a href="#_"
+                        class="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
+                        <span class="mx-auto text-xl font-black leading-none text-gray-900 select-none">OTA PageBuilder<span
+                                class="text-indigo-600">.</span></span>
+                    </a>
+                </div>
+                <div class="inline-flex items-center ml-5 space-x-2 lg:justify-end">
+                    <ul class="flex gap-x-4">
+                        <li><nuxt-link class="font-bold" to="/">Home</nuxt-link></li>
+                        <li><nuxt-link class="font-bold" to="/page-builder/editor">Editor</nuxt-link></li>
+                        <li><nuxt-link class="font-bold" to="/page-builder/preview">Preview</nuxt-link></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <div class="container mx-auto"></div>
+    </div>
+</template>
